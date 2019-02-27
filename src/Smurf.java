747d9c3f23b9e93d2ffb9170668672c266b1cb0a
League <league@WTS9.attlocal.net>
2019-02-26 18:30:12 -0800

    harry Potter- and smurf (incomplete)

--- src/Smurf.java
+++ src/Smurf.java
@@ -4,8 +4,8 @@
  */
 
 /* 
- * 1. Watch this Smurf cartoon: https://www.youtube.com/watch?v=RqbpzEHuO2g
- * 2. In a Runner class, make a Handy Smurf. Use the methods below to make him eat, and print his name.
+ * 1. Watch this Smurf cartoon: https://www.youtube.com/watch?v=RqbpzEHuO2g:P
+ * 2. In a Runner class, make a Handy Smurf. Use the methods below to make him eat, and print his name. :P
  * 3&4 see comments in code below
  * 5. Make a Papa Smurf and print his name, hat color and girl or boy.
  * 6. Make a Smurfette and print her name, hat color and girl or boy. 
@@ -13,7 +13,7 @@
 
 public class Smurf {
 
-	private String name;
+	private String name;                                  
 
 	Smurf(String name) {
 		this.name = name;
@@ -30,13 +30,19 @@ public class Smurf {
 	/* Papa Smurf wears a red hat, all the others are white. */
 	public String getHatColor() {
 		// 3. Fill in this method
-		return "";
+		if(Smurfette) {
+			return "white hat";
+		}
+		return "RED";
 	}
 
 	/* Smurfette is the only female Smurf. */
 	public String isGirlOrBoy() {
 		// 4. Fill in this method
-		return "";
+		if(Smurfette) {
+			return "Girl";
+		}
+		return "BOY";
 	}
 
 }
---
+++ src/SmurfRunner.java
@@ -0,0 +1,22 @@
+
+public class SmurfRunner {
+	public static void main(String[] args) {
+		Smurf Smurf = new Smurf("Smurf");
+		
+		Smurf.eat();
+		
+		System.out.println("Smurf");
+		
+		Smurf papaSmurf = new Smurf("papaSmurf");
+		
+		System.out.println("papaSmurf");
+		
+	System.out.println(	papaSmurf.isGirlOrBoy());
+		
+		System.out.println(papaSmurf.getHatColor());
+		
+		Smurf Smurfette = new Smurf("Smurfette");
+		
+	}
+	
+}
