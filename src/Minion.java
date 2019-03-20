63cd9cf0f000500e46418f87e3b7ae5f398549b2
rtgamer8 <league@WTS22.attlocal.net>
2019-03-19 18:25:11 -0700

    Did Minion:)

--- src/Minion.java
+++ src/Minion.java
@@ -7,8 +7,47 @@ public class Minion {
 	private String master;
 
 	Minion(String name, int eyes, String color, String master) {
-		
+
+		this.name = name;
+		this.eyes = eyes;
+		this.color = color;
+		this.master = master;
 	}
 
-	String name()
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String mame) {
+
+		this.name = name;
+	}
+
+	public int getEyes() {
+		return this.eyes;
+	}
+
+	public void setEyes(int eyes) {
+
+		this.eyes = eyes;
+	}
+
+	public String getColor() {
+		return this.color;
+	}
+
+	public void setColor(String color) {
+
+		this.color = color;
+	}
+
+	public String getMaster() {
+		return this.master;
+	}
+
+	public void setMaster(String master) {
+
+		this.master = master;
+	}
+
 }
